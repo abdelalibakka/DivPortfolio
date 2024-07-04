0587336ccec84f9f0436ef3b5d3fc8862584c1d4
Muigai-ben <muigaibenson37@gmail.com>
2024-07-04 10:23:40 +0000

    mangodb-connection

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5003;
 
 // Middleware
 app.use(cors());
